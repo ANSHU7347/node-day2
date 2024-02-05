f550bf9aa90bcef6806f56254dadd7bc4f0edd7c
ANSHU7347 <agrahari15092001@gmail.com>
2024-02-05 22:49:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//const colors=require('colors');
+//console.log('hello'.green);
+
+//console.log("package.json");
+console.log("Try nodemon");
+/*
+const http=require('http');
+http.createServer((resp,req)=>{
+
+}).listen(6000);
+*/
